ab8925bb4f2e95cc3e4210578784483778930a59
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2022-12-11 23:19:01 +0100

    Remove User endpoint from money-related project

--- cost-accounting-money-actions-core/src/main/java/com/posadskiy/costaccounting/moneyactions/core/controller/UserController.java
+++ cost-accounting-money-actions-core/src/main/java/com/posadskiy/costaccounting/moneyactions/core/controller/UserController.java
@@ -10,17 +10,10 @@ import java.util.List;
 
 public interface UserController {
 	DbUser getById(String userId);
-	User getMappedById(String userId);
-	DbUser getByEmail(String email);
-	DbUser getByChatId(Long chatId);
 	DbUser save(@NotNull DbUser dbUser);
 	void savePurchase(String userId, DbPurchase dbPurchase);
 	void saveIncome(String userId, DbIncome dbIncome);
-	List<DbPurchase> lastPurchases(String userId);
-	List<DbIncome> lastIncomes(String userId);
 	DbUser deletePurchase(String userId, String purchaseId);
 	DbUser deleteIncome(String userId, String incomeId);
-	List<User> getAllUsersByUserId(String userId);
-	List<DbUser> getAllUsersByProjectId(String userId);
-	User updateUser(User user);
+
 }
